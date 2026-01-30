da0838117f778cf3929365f61da832ddfbc3ad40
YaredAlex <yareda25@gmail.com>
2026-01-30 10:19:01 +0300

    alternate: iterative solution

--- all_path_from_source_to_target.py
+++ all_path_from_source_to_target.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+from collections import defaultdict,deque
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
         # let's do dfs to traverse to all possible ways to reach n-1
@@ -10,16 +10,31 @@ class Solution:
             adjacency[i].extend(graph[i])
         ans = []
         #recursive method
-        def dfs(node,path):
-            if node==(len(graph)-1):
-                ans.append(path[:])
+        # def dfs(node,path):
+        #     if node==(len(graph)-1):
+        #         ans.append(path[:])
 
+        #     for n in adjacency[node]:
+        #         path.append(n)
+        #         dfs(n,path)
+        #         path.pop()
+        # dfs(0,[0])
+        # print(ans)
+        # return ans
+
+        # Iterative method
+        #iterative method
+        #The idea is to keep track of path when traversing to different node (it similar to dfs but faster)
+        q = deque()
+        q.append((0,[0]))
+        while  q:
+            node,path = q.popleft()
+            if node==(len(graph)-1):
+                ans.append(path)
             for n in adjacency[node]:
-                path.append(n)
-                dfs(n,path)
-                path.pop()
-        dfs(0,[0])
-        print(ans)
+                q.append((n,path+[n]))
+        
         return ans
+
     
 
