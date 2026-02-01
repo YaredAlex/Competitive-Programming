cbdd4c90c1f2b4fa6c5483156045c6f45156f2bd
YaredAlex <yareda25@gmail.com>
2026-02-01 19:09:21 +0300

    perf(sol): using the first element before sorting and the first two element after sorting

--- divide_array_into_subarray_with_minimum_cost.py
+++ divide_array_into_subarray_with_minimum_cost.py
@@ -8,4 +8,12 @@ class Solution:
             for j in range(i+1,len(nums)):
                 cost = nums[0]+nums[i]+nums[j]
                 ans = min(ans,cost)
+
+        # optimized solution
+        # get the first element since it is not going to be changed
+        first = nums[0]
+        # sort the array
+        nums.sort()
+        # return the sum of the first two number in sorted array and first number before sorting 
+        # return first+nums[0]+nums[1]
         return ans
